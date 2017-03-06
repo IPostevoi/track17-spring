81c974b6406af2cade8dc253aa71761293813c34
IPostevoi <baklandor410@yandex.ru>
2017-03-06 03:35:59 +0300

    3rd try

--- src/main/java/track/lessons/lesson1/CountWords.java
+++ src/main/java/track/lessons/lesson1/CountWords.java
@@ -42,8 +42,8 @@ public class CountWords {
      * @return - целое число - сумма всех чисел из файла
      */
     public long countNumbers(File file) throws Exception {
-        BufferedReader br = new BufferedReader( new FileReader("/home/bakla410/Desktop/" + //
-                                                                        "TechnotrackJava/track17/words.txt"));
+        String dirPath = file.getAbsolutePath();
+        BufferedReader br = new BufferedReader( new FileReader(dirPath));
         String line;
         int sum = 0;
         while ((line = br.readLine()) != null) {
@@ -63,8 +63,8 @@ public class CountWords {
      * @return - результирующая строка
      */
     public String concatWords(File file) throws Exception {
-        BufferedReader br = new BufferedReader( new FileReader("/home/bakla410/Desktop/" + //
-                                                                        "TechnotrackJava/track17/words.txt"));
+        String dirPath = file.getAbsolutePath();
+        BufferedReader br = new BufferedReader( new FileReader(dirPath));
         String line;
         String string = "";
         while ((line = br.readLine()) != null) {
@@ -75,9 +75,9 @@ public class CountWords {
         return string;
     }
 
-    public static void main(String[] args) throws Exception {
+    /**public static void main(String[] args) throws Exception {
         CountWords test = new CountWords();
-        String dirPath = "/home/bakla410/Desktop/TechnotrackJava/track17/words.txt";
+        String dirPath = ;
         File file = new File(dirPath);
         long count = test.countNumbers(file);
         String str = test.concatWords(file);
@@ -85,5 +85,6 @@ public class CountWords {
         System.out.println(str);
 
     }
+    **/
 }
 
